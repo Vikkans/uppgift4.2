c70e5e296f954fe5a17ff1efde974575117774bc
Vikkans <Jakobsenviktor6@gmail.com>
2023-10-18 10:50:28 +0200

    Add project files.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string svar = Console.ReadLine();
+            int heltal = int.Parse(svar);
+
+            while (true)
+            {
+                Console.WriteLine("Vill du skriva in ett till heltal?");
+                string svar2 = Console.ReadLine();
+                if (svar2 == "nej" || svar2 == "Nej")
+                {
+                    Console.WriteLine("Ditt största tal är " + heltal);
+                    break;
+                    
+           
+                }
+                if (svar2 == "ja" || svar2 == "Ja")
+                {
+                    Console.WriteLine("skriv in ett heltal");
+                    int heltal2 = int.Parse(Console.ReadLine());
+                    if (heltal2 > heltal)
+                    {
+                        heltal = heltal2;
+                    }
+                }
+                Console.ReadKey();
+            }            
+        }
+    }
+}
